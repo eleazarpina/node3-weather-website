7168e6ce00ab43822a6984e5526a00c6c547dc60
Eleazar Pina <epina@coipsa.cl>
2020-04-27 16:20:29 -0400

    Init commit

---
+++ src/app.js
@@ -0,0 +1,149 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Ema Pina'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Weather App',
+        name: 'Ema Pina'
+    })
+})
+
+
+app.get('/weather', (req, res) => {
+
+    if (!req.query.address) {
+        return res.send({
+            error: 'Address must be provided'
+        })
+    }
+
+
+    // latitude longi location se reemplazzan en el callback destructuring
+    geocode(req.query.address, (error, { latitude, longitude, location } = {} ) => {
+
+        if (error) {
+            return res.send({
+                error
+            })
+        } else {
+            // console.log('Error', error)
+            // console.log('Data', data)
+
+            forecast(latitude, longitude, (error, forecastData) => {
+
+                if (error) {
+                return res.send({
+                    error
+                    })
+                }
+                
+
+                // const {latitude, longitude, location} = data            
+
+                // console.log('Error', error)
+                // console.log('GeoData', latitude, longitude, location)
+                // console.log('ForecastData', forecastData)
+
+                res.send({
+                    forecast: forecastData,
+                    location: location,
+                    address: req.query.address
+                })
+            }) 
+        }
+
+    })
+
+
+
+    
+})
+
+app.get('/products', (req, res) => {
+    res.send({
+        products: [ 'Va a nevar', 'leche', 'huevos']
+        
+    })
+})
+
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helptext: 'This is some helpful text',
+        title: 'Help',
+        name: 'Ema Pina'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {        
+        title: 'Page not found',
+        errorMessage: 'Help article not found',
+        name: 'Ema Pina'
+    })    
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {        
+        title: '404',
+        errorMessage: 'Page not found',
+        name: 'Ema Pina'
+    })    
+})
+
+// app.get('', (req, res) => {
+//     res.send('<h1>Weather</h1>')
+// })
+
+// app.get('/help', (req, res) => {    
+//     // res.send('Help page')
+//     // res.send({
+//     //     Name: 'Ema',
+//     //     Age: 2
+//     // })
+//     // res.send([ {
+//     //     name: 'Ema'
+//     // }, {
+//     //     name: 'Carolina'
+//     // }, {
+//     //     name: 'Eleazar'
+//     // }])
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<html><head><title>ABOOOOOUT</title></head><body><h1>About</h1></body></html>')
+// })
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,34 @@
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+
+    const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=652c7a9f6ea1c063fa4fa09fd8ada9f9'
+
+    // destructuring url .... response como sólo ocupa body, vamos cambiando
+    request({ url, json: true}, (error, { body }) => {
+        // console.log(response.body) 
+        if (error) {
+            callback('Unable to connect to weather service', undefined)
+        }  else if (body.error) {
+            callback('Unable to find location', undefined)
+        }  else if (body.message) {
+            callback(body.message, undefined)
+        } else {
+            callback(undefined, body.main.temp)
+        }
+    })
+
+
+}
+
+module.exports = forecast                   
